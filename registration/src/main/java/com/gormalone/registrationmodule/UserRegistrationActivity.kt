6917c9a3bebc54cba13be0abd280817463649879
anand <ra.anand2k>
2021-05-11 12:32:58 +0530

    Initial Commit

---
+++ registration/src/main/java/com/gormalone/registrationmodule/UserRegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.gormalone.registrationmodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserRegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
