ca15dc3bee114e5d88506dd6cdc5e03f265b7605
anand <ra.anand2k>
2021-05-11 14:37:35 +0530

    Updated 3

--- registration/src/main/java/com/gormalone/registrationmodule/NewUserRegistrationActivity.kt
+++ registration/src/main/java/com/gormalone/registrationmodule/NewUserRegistrationActivity.kt
@@ -2,12 +2,10 @@ package com.gormalone.registrationmodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
-class UserRegistrationActivity : AppCompatActivity() {
+class NewUserRegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.i("UserRegistrationActivity", "onCreate")
+        setContentView(R.layout.activity_newuserregistration)
     }
 }
